f62914c1ffe54a977eed34df094644faa796ab73
Arap <arttu.siren@gmail.com>
2016-11-22 17:35:52 +0200

    SQL kommentit pois jotta parseeminen onnistuu

--- src/main/resources/sql/add_test_data.sql
+++ src/main/resources/sql/add_test_data.sql
@@ -1,17 +1,16 @@
--- Asiakas-taulun testidata
-INSERT INTO Asiakas (kayttajanimi, salasana, syntymaaika, sukupuoli, liittynyt, osoite) 
+INSERT INTO Asiakas (kayttajanimi, salasana, syntymaaika, sukupuoli, liittynyt, osoite)
 VALUES ('Toto99112', 'salainensalasana','04.11.1997',1,'15.11.2016','jokukatu 11C, Espoo, Suomi');
 
 INSERT INTO Asiakas (kayttajanimi, salasana, syntymaaika, sukupuoli, liittynyt, osoite) 
 VALUES ('Gajaja2828', 'salainensalasana2','08.01.1993',1,'15.11.2016','jokukatu 12C, Espoo, Suomi');
 
--- Esittelysivu-taulun testidata
+
 INSERT INTO Esittelysivu (omistaja_id, otsikko, leipateksti, luotu, muokattu, julkinen) 
 VALUES (1, 'Etsin jalkapallokaveria','Hei! Etsin kaikenikäistä kaveria pelaamaan kanssani jalkapalloa','15.11.2016','15.11.2016', true);
 
 INSERT INTO Esittelysivu (omistaja_id, otsikko, leipateksti, luotu, muokattu, julkinen) 
 VALUES (2, 'Etsin juttukaveria','Hei! Olen 27 vuotias ja kaipaan juttukaveria! laita viestiä!','15.11.2016','15.11.2016', true);
 
--- Viesti-taulun testidata
+
 INSERT INTO Viesti (lahettaja, vastaanottaja, lahtetty, sisalto) 
 VALUES (2, 1,'15.11.2016','Hei!  Vaikutat mukavalta! ');
