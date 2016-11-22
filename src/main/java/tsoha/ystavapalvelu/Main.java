abe34443ac7e4946cc5d9562b3128e63eeda37c4
Arap <arttu.siren@gmail.com>
2016-11-22 18:03:28 +0200

    fix bugs

--- src/main/java/tsoha/ystavapalvelu/Main.java
+++ src/main/java/tsoha/ystavapalvelu/Main.java
@@ -33,8 +33,8 @@ public class Main {
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            boolean kirjautunut = req.session().attribute("kayttaja") != null;
-            map.put("kirjautunut", kirjautunut);
+            boolean kirjautunut = req.session().attribute("asiakas") != null;
+            map.put("kirjautunut", !kirjautunut);
 
             if(kirjautunut) {
                 map.put("kayttaja", ( (Asiakas)req.session().attribute("asiakas")).getKayttajanimi());
